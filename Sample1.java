ba1fbfc2503eb9419a9f8baa0dd07aa6b0929e8f
bvrajugithub2023flask <bvraju.professional.memories@gmail.com>
2024-11-02 19:22:56 +0530

    Update Sample1.java
    New function fun2() added - It just displays a message on the console

--- Sample1.java
+++ Sample1.java
@@ -2,4 +2,8 @@ public class Sample1{
     public void fun1(){
         System.out.println("fun1 created by srikar");
     }
+
+    public void fun2(){
+        System.out.println("fun2 created by BVRaju");
+    }
 }
