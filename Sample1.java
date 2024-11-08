e365a03dd70e0ebbbe24c9034ebffc7a099e6e4f
Pradeep Jagarapu <117536420+PradeepJagarapu@users.noreply.github.com>
2024-11-08 14:47:28 +0530

    Update Sample1.java
    Changed by pradeep Andhra University

--- Sample1.java
+++ Sample1.java
@@ -6,4 +6,8 @@ public class Sample1{
     public void fun2(){
         System.out.println("fun2 created by BVRaju");
     }
+
+    public void fun3(){
+        System.out.println("fun3 created by Pradeep-lastclass");
+    }
 }
